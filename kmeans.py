91803e95e33b3d98f4e26d601f0fa7dd0887d404
s184190 <rasmusml@hotmail.dk>
2021-02-19 23:50:52 +0100

    loads linux shared lib only on linux platform

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,25 @@
+import sys
 from ctypes import *
 import numpy as np
 
-kmeans_lib = cdll.LoadLibrary("./bin/kmeans.so")
-kmeans = kmeans_lib.kmeans
-kmeans.argtypes = [c_int32, c_uint32]
-kmeans.restype = None
+def load_library():
+    global kmeans_lib
+    os = sys.platform
+
+    if os == "linux":
+        kmeans_lib = cdll.LoadLibrary("./bin/kmeans.so")
+    else:
+        print(f"{os} unsupported platform")
+        assert False
+
+def init_library():
+    global kmeans
+    kmeans = kmeans_lib.kmeans
+    kmeans.argtypes = [c_int32, c_uint32]
+    kmeans.restype = None
+
+
+load_library()
+init_library()
 
 kmeans(c_int32(-10), c_uint32(12))
